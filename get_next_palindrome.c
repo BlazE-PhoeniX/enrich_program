10be7f27720c27968fb8b373e62a610a39ab30cb
Hariprasath <53620434+BlazE-PhoeniX@users.noreply.github.com>
2021-01-05 19:26:18 +0530

    Revert "Revert "exercise 2""
    This reverts commit 73c908e1847c315390bcc80b291d209451dd022f.

---
+++ get_next_palindrome.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+int reverseNo(int number) {
+    int reverse=0;
+    while(number!=0) {
+        reverse = reverse*10 + (number%10);
+        number/=10;
+    }
+    return reverse;
+}
+
+int checkPalindrome(int number) {
+    return number==reverseNo(number);
+}
+
+int getNextPalindrome(int number) {
+    while(!checkPalindrome(number)) {
+        number++;
+    }
+    return number;
+}
+
+int main() {
+    int number, palindromeNo;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    
+    palindromeNo=getNextPalindrome(number);
+    printf("Next Palindrome number = %d\n\n", palindromeNo);
+}
