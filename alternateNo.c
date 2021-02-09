5f2110c61da17d12ce40213d8e7b6020d9b31bfc
BlazE-PhoeniX <hariprasathh01@gmail.com>
2021-02-09 17:01:47 +0530

    none

--- alternateNo.c
+++ alternateNo.c
@@ -3,14 +3,12 @@
 int main() {
     int odd=1, even=2, n=5;
     for(int i=1; i<=n; i++) {
+
+        int *var = (i%2==1) ? &odd : &even;
+
         for(int j=1; j<=i; j++) {
-            if(i%2==1) {
-                printf("%d ", odd);
-                odd+=2;
-            } else {
-                printf("%d ", even);
-                even+=2;
-            }
+                printf("%d ", *var);
+                (*var)+=2;
         }
         printf("\n");
     }
