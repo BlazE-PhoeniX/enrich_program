5c1ec0574c5c276798fca57d3a0b4aebddd9ded4
BlazE-PhoeniX <hariprasathh01@gmail.com>
2021-02-11 16:18:01 +0530

    none

--- Patterns/pattern2.c
+++ Patterns/pattern2.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 
 int main() {
-    int n=5, k;
-    for(int i=1; i<=n; i++) {
-        k=n;
-        for(int j=1; j<=i; j++) {
-            printf("%d ", k);
-            k--;
+    int n=5;
+    for(int i=n; i>=1; i--) {
+        for(int j=n; j>=i; j--) {
+            printf("%d ", j);
         }
         printf("\n");
     }
